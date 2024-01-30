ca854f9b19435e561c65d5f316efb2fa7c928ee2
chitramalika08 <157237302+chitramalika08@users.noreply.github.com>
2024-01-30 12:35:24 +0530

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,18 @@
+import './App.css';
+import Employee from './Employee';
+import EmployeeList from './Emplisting';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>React JS CRUD Opertations</h1>
+
+      <EmployeeList>
+
+      </EmployeeList>
+    </div>
+  );
+
+}
+
+export default App;
